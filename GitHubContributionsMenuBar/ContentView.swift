25fa47a457d0c9416774964dc4698d53efed5b87
SNQ-2001 <taishin05109@gmail.com>
2022-05-05 02:07:08 +0900

    Initial Commit

---
+++ GitHubContributionsMenuBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubContributionsMenuBar
+//
+//  Created by 宮本大新 on 2022/05/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHubContributionsMenuBar/GitHubContributionsMenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubContributionsMenuBarApp.swift
+//  GitHubContributionsMenuBar
+//
+//  Created by 宮本大新 on 2022/05/05.
+//
+
+import SwiftUI
+
+@main
+struct GitHubContributionsMenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
